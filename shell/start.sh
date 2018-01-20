8a70724ac992dd8813a718e2698a0f403ba8e969
Zhou Weitong <xml@live.com>
2018-01-20 10:43:24 +0800

    Signed-off-by: Zhou Weitong <xml@live.com>

--- shell/start.sh
+++ shell/start.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 echo "Start Zookeeper"
-cd /root/soft/kafka_$kafka_ver-1.0.0
-nohup bin/zookeeper-server-start.sh config/zookeeper.properties 2>&1 >> /root/logs/zookeeper.log &
+cd /root/soft/kafka_$kafka_ver-1.0.0/bin
+nohup zookeeper-server-start.sh ../config/zookeeper.properties 2>&1 >> /root/logs/zookeeper.log &
 echo "Starting Kafka"
-nohup bin/kafka-server-start.sh config/server.properties 2>&1 >> /root/logs/kafka.log &
-cd /root/soft
+nohup kafka-server-start.sh ../config/server.properties 2>&1 >> /root/logs/kafka.log &
+cd /root/soft/router
 echo "Starting router"
-nohup router/router -c router/router.conf 2>&1 >> /root/logs/router.log &
+nohup router -c router.conf 2>&1 >> /root/logs/router.log &
 echo "Starting logic"
-nohup logic/logic -c logic/logic.conf 2>&1 >> /root/logs/logic.log &
+cd /root/soft/logic
+nohup logic -c logic.conf 2>&1 >> /root/logs/logic.log &
 echo "Starting comet"
-nohup comet/comet -c comet/comet.conf 2>&1 >> /root/logs/comet.log &
+cd /root/soft/comet
+nohup comet -c comet.conf 2>&1 >> /root/logs/comet.log &
 echo "Starting job"
-nohup job/job -c job/job.conf 2>&1 >> /root/logs/job.log &
+cd /root/soft/job
+nohup job -c job.conf 2>&1 >> /root/logs/job.log &
 while true;
 do sleep 1;
 done;
