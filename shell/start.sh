7f6aa777b2e3a3ffd6f73efcbf8d2a055d10ea44
Zhou Weitong <xml@live.com>
2018-01-20 13:28:43 +0800

    Signed-off-by: Zhou Weitong <xml@live.com>

--- shell/start.sh
+++ shell/start.sh
@@ -2,20 +2,26 @@
 echo "Start Zookeeper"
 cd /root/soft/kafka_$kafka_ver-1.0.0/bin
 nohup ./zookeeper-server-start.sh ../config/zookeeper.properties 2>&1 >> /root/logs/zookeeper.log &
+sleep 5
 echo "Starting Kafka"
 nohup ./kafka-server-start.sh ../config/server.properties 2>&1 >> /root/logs/kafka.log &
+sleep 5
 cd /root/soft/router
 echo "Starting router"
 nohup ./router -c router.conf 2>&1 >> /root/logs/router.log &
+sleep 5
 echo "Starting logic"
 cd /root/soft/logic
 nohup ./logic -c logic.conf 2>&1 >> /root/logs/logic.log &
+sleep 5
 echo "Starting comet"
 cd /root/soft/comet
 nohup ./comet -c comet.conf 2>&1 >> /root/logs/comet.log &
+sleep 5
 echo "Starting job"
 cd /root/soft/job
 nohup ./job -c job.conf 2>&1 >> /root/logs/job.log &
+sleep 5
 echo "Starting demo"
 cd /root/examples/javascript
 nohup ./main 2>&1 >> /root/logs/demo.log &
