1c0d4e705acea12474197d4da935e7fdca4e8d45
Ken Green <xml@live.com>
2018-01-21 20:31:34 +0800

    change

--- docker/kafka/shell/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /root/kafka_$kafka_ver-1.0.0/bin
-nohup ./zookeeper-server-start.sh ../config/zookeeper.properties 2>&1 >> /root/logs/zookeeper.log &
-sleep 5
-nohup ./kafka-server-start.sh ../config/server.properties 2>&1 >> /root/logs/kafka.log &
-while true;
-do sleep 10;
-done;
