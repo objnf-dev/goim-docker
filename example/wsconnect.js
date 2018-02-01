f23523c51e6d945b57dd9ce720b61aa5d24b6b49
Zhou Weitong <xml@live.com>
2018-02-01 13:13:27 +0800

    Add Message-send over websocket demo

--- example/wsconnect.js
+++ example/wsconnect.js
@@ -1,16 +1,18 @@
-function start() {
-    var ip_in = document.getElementById("ip");
-    var port_in = document.getElementById("port");
-    window.ip = ip_in.value;
-    window.port = port_in.value;
-    console.log("Server is:" + window.ip + ":" + window.port);
-    link(window.ip, window.port);
-    return true;
-}
 window.message = [];
 
 function Client() {
 
+}
+Client.prototype.send_data = function(name, msg_body) {
+    window.ws.send(JSON.stringify({
+        'ver': 1,
+        'op': 4,
+        'seq': 1,
+        'body': {
+            "name": name,
+            "msg_body": msg_body
+        }
+    }));
 }
 Client.prototype.createConnect = function(max, delay, server, port) {
     var self = this;
@@ -23,14 +25,14 @@ Client.prototype.createConnect = function(max, delay, server, port) {
     var heartbeatInterval;
 
     function connect(server, port) {
-        var ws = new WebSocket('ws://' + server + ':' + port + '/sub');
+        window.ws = new WebSocket('ws://' + server + ':' + port + '/sub');
         var auth = false;
 
-        ws.onopen = function() {
+        window.ws.onopen = function() {
             getAuth();
         }
 
-        ws.onmessage = function(evt) {
+        window.ws.onmessage = function(evt) {
             var receives = JSON.parse(evt.data)
             for (var i = 0; i < receives.length; i++) {
                 var data = receives[i];
@@ -51,22 +53,24 @@ Client.prototype.createConnect = function(max, delay, server, port) {
             }
         }
 
-        ws.onclose = function() {
+        window.ws.onclose = function() {
             if (heartbeatInterval) clearInterval(heartbeatInterval);
             setTimeout(reConnect, delay);
         }
 
         function heartbeat() {
-            ws.send(JSON.stringify({
-                'ver': 1,
-                'op': 2,
-                'seq': 2,
-                'body': {}
-            }));
+            if ((window.ip == server_temp) && (window.port == port_temp))
+                window.ws.send(JSON.stringify({
+                    'ver': 1,
+                    'op': 2,
+                    'seq': 2,
+                    'body': {}
+                }));
+            else return false;
         }
 
         function getAuth() {
-            ws.send(JSON.stringify({
+            window.ws.send(JSON.stringify({
                 'ver': 1,
                 'op': 7,
                 'seq': 1,
@@ -84,9 +88,21 @@ Client.prototype.createConnect = function(max, delay, server, port) {
     }
 }
 
-function link(ip, port) {
-    var instance = new Client();
+function start() {
+    var ip_in = document.getElementById("ip");
+    var port_in = document.getElementById("port");
+    window.ip = ip_in.value;
+    window.port = port_in.value;
+    console.log("Server is:" + window.ip + ":" + window.port);
+    window.client = new Client();
     var MAX_CONNECT_TIME = 10;
     var DELAY = 15000;
-    instance.createConnect(MAX_CONNECT_TIME, DELAY, window.ip, window.port);
+    window.client.createConnect(MAX_CONNECT_TIME, DELAY, window.ip, window.port);
+    return true;
+}
+
+function msg_send() {
+    var user_name = document.getElementById("name_in");
+    var user_msg = document.getElementById("msg_in");
+    window.client.send_data(user_name.value, user_msg.value);
 }
